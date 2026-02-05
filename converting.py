cc5941c2cec02f456abae1b1b15422911a56e61d
ashishravath-dotcom <ashishravath@gmail.com>
2026-02-06 00:54:41 +0530

    Temperature Conversion

---
+++ converting.py
@@ -0,0 +1,7 @@
+# Celsius to Fahrenheit converter
+celsius = 50.0
+# Formula: F = (C * 9/5) + 32
+fahrenheit = (celsius * 9/5) + 32
+print("Temerature conversion")
+print("Celsius:", celsius)
+print("Fahrenheit:", fahrenheit)
