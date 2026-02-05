a8483fd0cd0bbc97ff45c45632a2fb2398f2aa24
ashishravath-dotcom <ashishravath@gmail.com>
2026-02-06 00:38:23 +0530

    price and tax

---
+++ # program to calculate final price inclu.py
@@ -0,0 +1,10 @@
+# program to calculate final price including tax
+price = 2000
+tax_rate = 15
+tax_amount = price * tax_rate / 100
+final_price = price + tax_amount
+print("Price calculation",)
+print("Price:", price)
+print("Tax Rate:", tax_rate, "%")
+print("Tax Amount:", tax_amount)
+print("Final Price:", final_price) 
