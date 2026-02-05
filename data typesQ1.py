5eae47e234537a5e4e575c8e86017aecae881b16
ashishravath-dotcom <ashishravath@gmail.com>
2026-02-06 01:22:32 +0530

    all variables

---
+++ data typesQ1.py
@@ -0,0 +1,23 @@
+# Python program to demonstrate main data types
+# 1. String
+name = "Ashish"
+print("Name:", name)
+# 2. Integer
+age = 25
+print("Age:", age)
+# 3. Float
+height = 5.9
+print("Height:", height)
+# 4. Boolean
+is_student = False
+print("Is Student:", is_student)
+# 5. List
+hobbies = ["Reading", "Traveling", "writing"]
+print("Hobbies:", hobbies)
+# 6. Dictionary (key-value pairs)
+person = {
+    "name": "Ashish",
+    "age": 25,
+    "city": "Hyderabad"
+}
+print("Person Details:", person)
