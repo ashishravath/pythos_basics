32d97c8584e4078c269ca3aedd4f723be161a299
ashishravath-dotcom <ashishravath@gmail.com>
2026-02-06 01:02:13 +0530

    simple interest

---
+++ simple interest.py
@@ -0,0 +1,11 @@
+#simple interest calculator
+principal = 4000
+rate = 3.5
+time = 7
+simple_interest = (principal * rate * time) / 100
+#total amount to be paid/received
+total_amount = principal + simple_interest
+print("Principal:", principal)
+print("Simple Interest:", simple_interest)
+print("Rate of Interest:", rate)
+print("Total Amount:", total_amount)
